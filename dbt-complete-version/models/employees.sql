93f5b596f290bcfcddbdadbfd32af23add56d1a0
Meigan Luiz Uliana <109111587+mluizuliana@users.noreply.github.com>
2023-08-07 08:04:44 -0300

    Update employees.sql

--- dbt-complete-version/models/employees.sql
+++ dbt-complete-version/models/employees.sql
@@ -1,8 +1,8 @@
 --model employees
 with calc_employees as (
     select 
-    date_part(year,current_date) - date_part(year, birth_date) age ,
-    date_part(year,current_date) - date_part(year,hire_date) lengthofservice,
+    date_part(year,current_date) - date_part(year, birth_date) age,
+    date_part(year,current_date) - date_part(year, hire_date) lengthofservice,
     first_name || ' ' || last_name name, *
     from {{source('sources','employees')}}
 )
